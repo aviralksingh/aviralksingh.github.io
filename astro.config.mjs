bd004dd454fdf17b191565953c34a9114de5af66
Aviral Singh <avsingh@zoox.com>
2025-05-12 10:29:33 -0700

    fix: correct base path for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import tailwind from '@astrojs/tailwind';
 export default defineConfig({
   integrations: [tailwind()],
   site: 'https://aviralksingh.github.io',
-  base: '/aviralksingh.github.io'
+  base: '/'
 });
